99681174eb82688789fcd54be5c538e28888eb1b
Alvin Yu <224910621+alvin-y-ly@users.noreply.github.com>
2025-08-27 14:27:15 +0900

    add tasks 8-10

---
+++ prognum.py
@@ -0,0 +1,12 @@
+def factorial(n: int) -> int:
+    if n < 2:
+        return 1
+    return n * factorial(n - 1)
+
+
+def fibonacci(n: int) -> int:
+    if n <= 0:
+        return -1
+    if n <= 2:
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
